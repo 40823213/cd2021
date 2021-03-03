2281f789a13e2d6d20199633793156d20da2dda0
40823213 <wangrex222@gmail.com>
2021-03-03 23:13:41 +0800

    leo change

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40823213'
+SITENAME = 'cd2021 40823213'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
